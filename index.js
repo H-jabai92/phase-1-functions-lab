2e05c04077272b64f6a29e90520ba3feab998f1d
Hasan <h.mjabai92@gmail.com>
2023-10-07 18:18:39 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(num) {
+    return num - 42
+}
+console.log(distanceFromHqInBlocks(50))
+
+
+function distanceFromHqInFeet(someValue) {
+   return distanceFromHqInBlocks(50)* 264 ;
+}
+console.log( distanceFromHqInFeet())
+ 
+  function distanceTravelledInFeet(start, destination) {
+    return (start-destination)*264
+}
+  console.log (distanceTravelledInFeet(38,34))
+
+//    
